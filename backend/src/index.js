ca3c7f235b19a2f506b94bb73ba03c2237365ba6
AyushPoojariUCD <ayush.poojari@ucdconnect.ie>
2026-03-09 23:08:24 +0530

    feat: websocket sportz frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,37 +1,48 @@
 import AgentAPI from "apminsight";
 AgentAPI.config();
 
-import express from 'express';
-import http from 'http';
-import {matchRouter} from "./routes/matches.js";
-import {attachWebSocketServer} from "./ws/server.js";
-import {securityMiddleware} from "./arcjet.js";
-import {commentaryRouter} from "./routes/commentary.js";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { matchRouter } from "./routes/matches.js";
+import { attachWebSocketServer } from "./ws/server.js";
+import { securityMiddleware } from "./arcjet.js";
+import { commentaryRouter } from "./routes/commentary.js";
 
 const PORT = Number(process.env.PORT || 8000);
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || "0.0.0.0";
 
 const app = express();
 const server = http.createServer(app);
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express server!');
+app.get("/", (req, res) => {
+  res.send("Hello from Express server!");
 });
 
 // app.use(securityMiddleware());
 
-app.use('/matches', matchRouter);
-app.use('/matches/:id/commentary', commentaryRouter);
+app.use("/matches", matchRouter);
+app.use("/matches/:id/commentary", commentaryRouter);
 
-const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
+const { broadcastMatchCreated, broadcastCommentary } =
+  attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
 app.locals.broadcastCommentary = broadcastCommentary;
 
 server.listen(PORT, HOST, () => {
-    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+  const baseUrl =
+    HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
 
-    console.log(`Server is running on ${baseUrl}`);
-    console.log(`WebSocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`);
+  console.log(`Server is running on ${baseUrl}`);
+  console.log(
+    `WebSocket Server is running on ${baseUrl.replace("http", "ws")}/ws`,
+  );
 });
